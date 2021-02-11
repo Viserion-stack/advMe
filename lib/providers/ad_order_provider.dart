2bf42f5faf513c5831ab3313331a56fde867e277
Viserion <64782694+Viserion-stack@users.noreply.github.com>
2021-02-11 11:53:14 +0100

    updated provider

--- lib/providers/ad_order_provider.dart
+++ lib/providers/ad_order_provider.dart
@@ -1,37 +1,43 @@
 import 'dart:io';
-import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 
-import 'package:image_picker/image_picker.dart';
+Future<void> addPost(
+    DateTime date, String title, String description, File _pickedImage) async {
+  var firebaseUser = FirebaseAuth.instance.currentUser.uid;
 
-Future<void> _addPost(DateTime date, String, description, String url, File _pickedImage ) async {
-    var firebaseUser = FirebaseAuth.instance.currentUser;
-    var rng = new Random();
-    var random = rng.nextInt(100);
+  print('FB USER: ' + firebaseUser.toString());
 
-    final ref = FirebaseStorage.instance
-        .ref()
-        .child('posts')
-        .child(firebaseUser.uid + '.jpg');
+  // var rng = new Random();
+  // var random = rng.nextInt(100);
 
-    await ref.putFile(_pickedImage);
-    
+  final ref = FirebaseStorage.instance
+      .ref()
+      .child('allAds')
+      .child(firebaseUser + '.jpg');
 
-    final url = await ref.getDownloadURL();
+  await ref.putFile(_pickedImage);
 
-    FirebaseFirestore.instance.collection('posts').doc().set({
-      'id': '$random',
-      'description': description,
-      'imageUrl': url,
-      'isFavorite': true,
-      'userId' : firebaseUser,
-    });
-    //description.clear();
-    
-    print('Adding photo...');
+  final url = await ref.getDownloadURL();
 
-    //Navigator.of(context).pop();
-  }
+  FirebaseFirestore.instance
+      .collection('allAds')
+      .doc(firebaseUser)
+      .collection(title)
+      .doc(title)
+      .set({
+    'Added on ': date,
+    'title': title,
+    'description': description,
+    'imageUrl': url,
+    'isFavorite': true,
+    'userId': firebaseUser,
+  });
+  //description.clear();
+
+  print('Adding photo...');
+
+  //Navigator.of(context).pop();
+}
