93b9523a8af802e8578f6230fb4a9fb82235fcb0
Viserion <64782694+Viserion-stack@users.noreply.github.com>
2021-02-19 09:22:26 +0100

    Fix map widget

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -5,22 +5,21 @@ import 'package:http/http.dart' as http;
 const GOOGLE_API_KEY = 'AIzaSyAeoBRNEW_2WDXj-3pYXO4C9tUl4xLbDCc';
 
 class LocationHelper {
-  static String generateLocationPreviewImage({double latitude, double longitude,}) {
+  static String generateLocationPreviewImage({
+    double latitude,
+    double longitude,
+  }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
   }
 
   static Future<String> getPlaceAddress(double lat, double lng) async {
-    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
     final response = await http.get(url);
     return json.decode(response.body)['results'][0]['formatted_address'];
   }
 
-
- static String generateLocationPreviewImagebyAddress(String address) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$address&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$address&key=$GOOGLE_API_KEY';
+  static String generateLocationPreviewImagebyAddress(String address) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$address&zoom=17&size=600x600&maptype=roadmap&markers=color:red%7Clabel:A%7C$address&key=$GOOGLE_API_KEY';
   }
-
-
 }
-
-
