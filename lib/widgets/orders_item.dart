73fa1aae0ed3a20a2a53f75dbff5fe8becc2afe1
Ringlex <bercik.1122@wp.pl>
2021-02-27 14:30:18 +0100

    Improved colors in open container

--- lib/widgets/orders_item.dart
+++ lib/widgets/orders_item.dart
@@ -38,10 +38,12 @@ class OrdersItem extends StatelessWidget {
     // ignore: unused_local_variable
     var username = FirebaseAuth.instance.currentUser.displayName.toString();
     return OpenContainer(
+      openElevation: 0,
+      closedElevation: 0,
       transitionDuration: Duration(milliseconds: 650),
       transitionType: ContainerTransitionType.fade,
-      closedColor: settings.isDark ?  Color(0x40303250) : Colors.transparent,
-      openColor: settings.isDark ? Color(0xFF171923): Colors.transparent,
+      closedColor: settings.isDark ? Colors.transparent : Color(0xFFE9ECF5),
+      openColor: settings.isDark ? Color(0xFF171923) : Color(0xFFE9ECF5),
       openBuilder: (context, _) => OrderDetailScreen(
         id: id,
         title: title,
@@ -53,106 +55,106 @@ class OrdersItem extends StatelessWidget {
         website: website,
         address: address,
       ),
-      closedBuilder: (context, _) => Container(
-        width: 500,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Card(
-              color: Color(0x40303250),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
+      closedBuilder: (context, _) => Padding(
+        padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Card(
+            color: settings.isDark ? Color(0x40303250) : Color(0x55387CFF),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            elevation: 4,
+            margin: EdgeInsets.all(2),
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+                bottomRight: Radius.circular(15),
               ),
-              elevation: 4,
-              margin: EdgeInsets.all(5),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                  bottomRight: Radius.circular(15),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: settings.isDark ? 30 : 30,
+                  sigmaY: settings.isDark ? 5 : 25,
                 ),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 30, sigmaY: 5),
-                  child: Row(
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          //topRight: Radius.circular(5),
-                          bottomLeft: Radius.circular(15),
-                        ),
-                        child: Image.network(
-                          imageUrl,
-                          height: 120,
-                          width: 120,
-                          fit: BoxFit.cover,
-                        ),
+                child: Row(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        //topRight: Radius.circular(5),
+                        bottomLeft: Radius.circular(15),
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Column(children: <Widget>[
-                          Text(
-                            title,
+                      child: Image.network(
+                        imageUrl,
+                        height: 130,
+                        width: 130,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(children: <Widget>[
+                        Text(
+                          title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Container(
+                          child: Text(
+                            '$price zł',
                             style: TextStyle(
-                              color: Colors.white,
+                              color: Color(0xFFC31331),
+                              fontSize: 26,
                               fontWeight: FontWeight.bold,
-                              fontSize: 22,
                             ),
                           ),
-                          SizedBox(
-                            height: 5,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                            child: Row(children: [
+                          Icon(
+                            Icons.star,
+                            color: Color(0xFFF79E1B),
                           ),
-                          Container(
-                            child: Text(
-                              '$price zł',
-                              style: TextStyle(
-                                color: Color(0xFFC31331),
+                          Icon(
+                            Icons.star,
+                            color: Color(0xFFF79E1B),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Color(0xFFF79E1B),
+                          ),
+                          Icon(
+                            Icons.star_border,
+                            color: Color(0xFFF79E1B),
+                          ),
+                          Icon(
+                            Icons.star_border,
+                            color: Color(0xFFF79E1B),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '3.2',
+                            style: TextStyle(
                                 fontSize: 26,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                                color: Color(0xFFF79E1B),
+                                fontWeight: FontWeight.w700),
                           ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Container(
-                              child: Row(children: [
-                            Icon(
-                              Icons.star,
-                              color: Color(0xFFF79E1B),
-                            ),
-                            Icon(
-                              Icons.star,
-                              color: Color(0xFFF79E1B),
-                            ),
-                            Icon(
-                              Icons.star,
-                              color: Color(0xFFF79E1B),
-                            ),
-                            Icon(
-                              Icons.star_border,
-                              color: Color(0xFFF79E1B),
-                            ),
-                            Icon(
-                              Icons.star_border,
-                              color: Color(0xFFF79E1B),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              '3.2',
-                              style: TextStyle(
-                                  fontSize: 26,
-                                  color: Color(0xFFF79E1B),
-                                  fontWeight: FontWeight.w700),
-                            ),
-                          ])),
-                        ]),
-                      ),
-                    ],
-                  ),
+                        ])),
+                      ]),
+                    ),
+                  ],
                 ),
               ),
             ),
